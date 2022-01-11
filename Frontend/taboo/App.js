3ff74b12c4f46561f086326a7e5bb3cbcb81c837
lazarpe <lazar.petrovic.nefti@gmail.com>
2022-01-11 15:33:15 +0100

    socket io connection

--- Frontend/taboo/App.js
+++ Frontend/taboo/App.js
@@ -1,14 +1,26 @@
 import React from "react-native";
 import Menu from "./screens/Menu";
-import { NavigationContainer } from "@react-navigation/native";
+import Join from "./screens/Join";
+import { NavigationContainer, StackActions } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const HomeStack = createNativeStackNavigator();
 
 export default function App() {
+  const io = require("socket.io-client");
+  const socket = io("http://server.davidemarcoli.de:9092/");
+  socket.connect();
+
   return (
     <NavigationContainer>
-      <Menu />
+      <Stack.Navigator>
+        <Stack.Screen
+          name="home"
+          component={Menu}
+          options={{ title: "Welcome" }}
+        />
+        <Stack.Screen name="Profile" component={Join} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
